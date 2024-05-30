894ced18ddaf9aedb0b4e1b05c7ada4a76918d5f
shabeena.syed <syedfarzana589@gmail.com>
2024-05-30 13:15:50 +0530

    arguments

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+names = ( "zaara" , "shabbu", "sonu")
+#lst of arrays 
+#index of the above array list
+ echo "fisrt movie is :  ${names[0]}" 
+  # indication f arrays is ()
+  #indication if list []
+echo "second name is : ${names[2]}"
