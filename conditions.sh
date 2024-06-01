5db262de3a6da86739560ab3a5e6cca41d3ea45a
shabeena.syed <syedfarzana589@gmail.com>
2024-06-01 21:30:33 +0530

    script

--- conditions.sh
+++ conditions.sh
@@ -8,3 +8,9 @@ else
 
 
 #-gt ,-lt, -ge , -le 
+ #i want to check a number is less than 20 or not
+  NUMBER  = $2
+  if [ $NUMBER -lt 20 ]
+  then
+   echo " given value is gretare thn 20"
+   else echo " given value is less than 20 "
