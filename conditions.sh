c4260b9fa2368ec0f4a2289e7e5430689fce2f4f
shabeena.syed <syedfarzana589@gmail.com>
2024-06-01 21:47:06 +0530

    script

--- conditions.sh
+++ conditions.sh
@@ -2,20 +2,20 @@
  NUMBER=$1 
 if  [ $NUMBER -gt 10 ] 
 then
-   echo " givenvalue is greater than 10"
+   echo " given no $NUMBER is greater than 10"
  
 else 
-   echo "given vlue is les than 10"
+   echo "given no $NUMBER is les than 10"
 fi
 
 #-gt ,-lt, -ge , -le 
  #i want to check a number is less than 20 or not
-  NUMBER=$2
-  if [ $NUMBER -lt 20 ] 
+ # NUMBER=$2
+  #if [ $NUMBER -lt 20 ] 
 
-  then
-    echo " given value is gretare thn 20"
+#  then
+ #   echo " given value is gretare thn 20"
   
-  else 
-    echo " given value is less than 20 "
-  fi
+ # else 
+  #  echo " given value is less than 20 "
+#  fi 
