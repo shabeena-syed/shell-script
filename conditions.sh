c6ad78ee075c559b7e768cf12870020cd5dad1c6
shabeena.syed <syedfarzana589@gmail.com>
2024-06-01 21:32:31 +0530

    script

--- conditions.sh
+++ conditions.sh
@@ -12,5 +12,6 @@ else
   NUMBER  = $2
   if [ $NUMBER -lt 20 ]
   then
-   echo " given value is gretare thn 20"
-   else echo " given value is less than 20 "
+    echo " given value is gretare thn 20"
+  else 
+    echo " given value is less than 20 "
