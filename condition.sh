d7b7d267cd370fc8538a7d54954d920f6045d106
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 19:02:34 +0530

    commits

--- condition.sh
+++ condition.sh
@@ -7,12 +7,14 @@ else
    echo "u are asuper user"
 fi
  
- dnf install nginx -y
+ dnf installl nginx -y
   if [ $? -ne 0]
     echo " instllation of nginx is .....FAILURE"
   else 
     echo " server is installing"
   fi
+
+  dnf install git -y
 
 
 
