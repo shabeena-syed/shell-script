185b53179981142119eb17122f9d2319cee22fc9
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 13:03:44 +0530

    commits

--- conditions.sh
+++ conditions.sh
@@ -2,7 +2,7 @@
  NUMBER=$80
  
 if [ $NUMBER -gt 80 ]
-echo -p "please enter your number" 
+read -p "please enter your number" 
 then
    echo " given no $NUMBER is greater than 80"
  
@@ -12,13 +12,13 @@ fi
 
 #-gt ,-lt, -ge , -le 
  #i want to check a number is less than 20 or not
-NUMBER=$2
-if [ $NUMBER -lt 2 ]
-echo -p "please enter your number"
-then
-   echo "given value is less than 2"
+# NUMBER=$2
+# if [ $NUMBER -lt 2 ]
+# read -p "please enter your number"
+# then
+#    echo "given value is less than 2"
 
-else 
-   echo "given value is greater thn 2" 
+# else 
+#    echo "given value is greater thn 2" 
 
-fi
+# fi
