6463afb1723ca0461de18f444a9b256d2c2e6369
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 12:52:16 +0530

    commits

--- conditions.sh
+++ conditions.sh
@@ -12,14 +12,11 @@ fi
 #-gt ,-lt, -ge , -le 
  #i want to check a number is less than 20 or not
 NUMBER=$2
-if [ $NUMBER -lt 20 ]{
-
+if [ $NUMBER -lt 20 ]
 then
    echo " given value is gretare thn 20"
-  
-}  
-else {
-   echo " given value is less than 20 "
-}   
+
+else 
+   echo " given value is less than 20 " 
 
 fi
