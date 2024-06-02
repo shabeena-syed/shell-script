04bc146fa4961c9a16a01948a5ca4409a5507cc3
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 15:58:32 +0530

    commits

--- condition.sh
+++ condition.sh
@@ -2,9 +2,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "please run with root user"
+   exit1
 else
    echo "u are asuper user"
 fi
 
 dnf install mysql -y
 echo " script is proceeding"
+
+
+# disadvantage 
+# -------------
+# it will not stop evn if is face error
