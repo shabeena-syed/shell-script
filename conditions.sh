9bc44a7b0ba30d30f33a1f69b880999e1508d466
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 12:50:00 +0530

    commits

--- conditions.sh
+++ conditions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
  NUMBER=$80
+ 
 if [ $NUMBER -gt 10 ] 
 then
    echo " given no $NUMBER is greater than 10"
@@ -10,7 +11,7 @@ fi
 
 #-gt ,-lt, -ge , -le 
  #i want to check a number is less than 20 or not
-  NUMBER=$2
+NUMBER=$2
 if [ $NUMBER -lt 20 ]{
 
 then
