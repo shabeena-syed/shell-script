52a9b0f5f4339d8fa63fd85830707a98545e8812
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 13:05:52 +0530

    commits

--- conditions.sh
+++ conditions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
- NUMBER=$80
+ NUMBER=$?
  
-if [ $NUMBER -gt 80 ]
+if [ $NUMBER -gt $? ]
 read -p "please enter your number" 
 then
-   echo " given no $NUMBER is greater than 80"
+   echo " given number is greater than 80"
  
 else 
-   echo "given no $NUMBER is les than 80"
+   echo "given number is les than 80"
 fi
 
 #-gt ,-lt, -ge , -le 
