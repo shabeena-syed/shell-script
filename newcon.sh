a58a225f73922d03ba9862e8bf42485b656dd811
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 13:40:18 +0530

    commits

---
+++ newcon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+NUMBER=$70
+read -p "please enter your number": USER_NUMBER 
+
+if [ "$USER_NUMBER" -gt "$NUMBER" ]
+then
+    echo "given number is grater then $NUMBER" 
+else 
+    echo "given number is les then $NUMBER"
+fi 
