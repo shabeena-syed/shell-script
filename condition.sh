1eefbced79cb95bacfc5e7ae65036144b0b3fb9a
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 15:48:05 +0530

    commits

--- condition.sh
+++ condition.sh
@@ -1,8 +1,9 @@
-USERID= $(id -u)
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-  echo "please run with root user"
+   echo "please run with root user"
 else
- echo "u are asuper user"
+   echo "u are asuper user"
 fi
+
 dnf install mysql -y
