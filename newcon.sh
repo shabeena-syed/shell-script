a70634cc5d2a8a6f395120586ebeb768aa20d43c
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 13:54:14 +0530

    commits

--- newcon.sh
+++ newcon.sh
@@ -2,11 +2,11 @@
 NUMBER=70
 read -p "please enter your number: " USER_NUMBER 
 
-if [ "$USER_NUMBER" -gt "$NUMBER" ]
-then
-    echo "given number is grater then $NUMBER" 
-else 
-    echo "given number is les then $NUMBER"
-else 
-    echo "given number is eq to $NUMBER"
-fi 
+if [ "$USER_NUMBER" -gt "$NUMBER" ]; then
+    echo "given number is greater than $NUMBER" 
+elif [ "$USER_NUMBER" -lt "$NUMBER" ]; then 
+    echo "given number is less than $NUMBER"
+else
+    echo "given number is equal to $NUMBER"
+fi
+
