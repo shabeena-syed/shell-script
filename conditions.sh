eef4a8aab30166069e22ef65cb6cf94758362545
shabeena.syed <syedfarzana589@gmail.com>
2024-06-02 13:00:02 +0530

    commits

--- conditions.sh
+++ conditions.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
  NUMBER=$80
  
-if [ $NUMBER -gt 10 ] 
+if [ $NUMBER -gt 80 ]
+echo -p "please enter your number" 
 then
-   echo " given no $NUMBER is greater than 10"
+   echo " given no $NUMBER is greater than 80"
  
 else 
-   echo "given no $NUMBER is les than 10"
+   echo "given no $NUMBER is les than 80"
 fi
 
 #-gt ,-lt, -ge , -le 
  #i want to check a number is less than 20 or not
 NUMBER=$2
-if [ $NUMBER -lt 20 ]
+if [ $NUMBER -lt 2 ]
+echo -p "please enter your number"
 then
-   echo "given value is gretare thn 20"
+   echo "given value is less than 2"
 
 else 
-   echo "given value is less than 20" 
+   echo "given value is greater thn 2" 
 
 fi
